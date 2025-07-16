ac4608bb4b7e9bc91fab272ed71cf2e4a5e16ea2
Agraw Mindaye <agraw@Aminats-MacBook-Pro.local>
2025-07-16 12:52:15 -0400

    fix: replace <a> tags with Next.js <Link> component in Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,14 +1,16 @@
 // components/Navbar.tsx
 
+import Link from 'next/link'
+
 export default function Navbar() {
     return (
       <nav className="w-full bg-white shadow p-4">
         <div className="max-w-4xl mx-auto flex justify-between">
           <span className="font-bold text-lg">Agraw Mindaye</span>
           <div className="space-x-4">
-            <a href="/" className="text-gray-700 hover:text-black">Home</a>
-            <a href="/projects" className="text-gray-700 hover:text-black">Projects</a>
-            <a href="/contact" className="text-gray-700 hover:text-black">Contact</a>
+            <Link href="/" className="text-gray-700 hover:text-black">Home</Link>
+            <Link href="/projects" className="text-gray-700 hover:text-black">Projects</Link>
+            <Link href="/contact" className="text-gray-700 hover:text-black">Contact</Link>
           </div>
         </div>
       </nav>
