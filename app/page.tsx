825ce08cc0f6b0cb07bf9153905d11c764b9e150
Agraw Mindaye <agraw@Aminats-MacBook-Pro.local>
2025-08-15 15:39:54 -0400

    chore(page): add Contact component to page

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Hero from '@/components/Hero'
 import Navbar from '@/components/Navbar'
 import About from '@/components/About'
 import Projects from '@/components/Projects'
+import Contact from '@/components/Contact'
 
 export default function Home() {
   return (
@@ -24,7 +25,7 @@ export default function Home() {
           <Projects />
         </section>
         <section id="contact" className="scroll-mt-20">
-          {/* ... */}
+          <Contact />
         </section>
       </div>
     </>
